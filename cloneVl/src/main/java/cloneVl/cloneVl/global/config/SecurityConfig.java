088af2e50dd23eb937e68e0df52fa3a1e2acbcf1
dh1010a <dh1010a@naver.com>
2023-07-02 20:40:58 +0900

    update securityconfig

--- cloneVl/src/main/java/cloneVl/cloneVl/global/config/SecurityConfig.java
+++ cloneVl/src/main/java/cloneVl/cloneVl/global/config/SecurityConfig.java
@@ -3,6 +3,9 @@ package cloneVl.cloneVl.global.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig{
@@ -12,6 +15,16 @@ public class SecurityConfig{
         http
                 .formLogin().disable() // formLogin 인증 비활성화
                 .httpBasic().disable() // httpBasic 인증 비활성화
-                .csrf().disable();
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeHttpRequests().requestMatchers("/login","/signUp","/")
+                .permitAll()
+                .anyRequest().authenticated();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
