5a693aa6ff411180613b669dcbd8fb679fed7960
dh1010a <dh1010a@naver.com>
2023-07-07 13:58:23 +0900

    modi Security after json

--- cloneVl/src/main/java/cloneVl/cloneVl/global/config/SecurityConfig.java
+++ cloneVl/src/main/java/cloneVl/cloneVl/global/config/SecurityConfig.java
@@ -1,12 +1,17 @@
 package cloneVl.cloneVl.global.config;
 
+import cloneVl.cloneVl.global.login.filter.JsonUsernamePasswordAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 
 @Configuration
 public class SecurityConfig{
@@ -23,6 +28,7 @@ public class SecurityConfig{
                 .authorizeHttpRequests().requestMatchers("/login","/signUp","/")
                 .permitAll()
                 .anyRequest().authenticated();
+        http.addFilterAfter(jsonUsernamePasswordLoginFilter(), LogoutFilter.class);
         return http.build();
 
     }
@@ -30,6 +36,23 @@ public class SecurityConfig{
     @Bean
     public PasswordEncoder passwordEncoder(){
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() {//2 - AuthenticationManager 등록
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();//DaoAuthenticationProvider 사용
+        provider.setPasswordEncoder(passwordEncoder());//PasswordEncoder로는 PasswordEncoderFactories.createDelegatingPasswordEncoder() 사용
+        //provider.setUserDetailsService(loginService); //이후 작성할 코드입니다.
+        return new ProviderManager(provider);
+    }
+
+    @Bean
+    public JsonUsernamePasswordAuthenticationFilter jsonUsernamePasswordLoginFilter(){
+        JsonUsernamePasswordAuthenticationFilter jsonUsernamePasswordLoginFilter = new JsonUsernamePasswordAuthenticationFilter(objectMapper);
+        jsonUsernamePasswordLoginFilter.setAuthenticationManager(authenticationManager());
+        //jsonUsernamePasswordLoginFilter.setAuthenticationSuccessHandler(loginSuccessJWTProvideHandler());
+        //jsonUsernamePasswordLoginFilter.setAuthenticationFailureHandler(loginFailureHandler());
+        return jsonUsernamePasswordLoginFilter;
     }
 
 }
